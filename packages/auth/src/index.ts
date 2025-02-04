73d7a7aa13447e665e34fab26ce1e40e116a436a
Cleiton Ambrosini <cleiton.ambro@gmail.com>
2025-02-04 20:33:40 -0300

    feat: create more permissions, models and subjects

--- packages/auth/src/index.ts
+++ packages/auth/src/index.ts
@@ -4,12 +4,25 @@ import {
   createMongoAbility,
   MongoAbility,
 } from '@casl/ability';
+import { z } from 'zod';
 import { User } from './models/user';
 import { permissions } from './permissions';
-import { ProjectSubject } from './subjects/project';
-import { UserSubject } from './subjects/user';
+import { billingSubject } from './subjects/billing';
+import { inviteSubject } from './subjects/invite';
+import { organizationSubject } from './subjects/organization';
+import { projectSubject } from './subjects/project';
+import { userSubject } from './subjects/user';
 
-type AppAbilities = UserSubject | ProjectSubject | ['manage', 'all'];
+const appAbilitiesSchema = z.union([
+  projectSubject,
+  userSubject,
+  organizationSubject,
+  inviteSubject,
+  billingSubject,
+  z.tuple([z.literal('manage'), z.literal('all')]),
+]);
+
+type AppAbilities = z.infer<typeof appAbilitiesSchema>;
 
 export type AppAbility = MongoAbility<AppAbilities>;
 export const createAppAbility = createMongoAbility as CreateAbility<AppAbility>;
@@ -23,7 +36,11 @@ export const defineAbilityFor = (user: User) => {
 
   permissions[user.role](user, builder);
 
-  const ability = builder.build();
+  const ability = builder.build({
+    detectSubjectType(subject) {
+        return subject.__typename
+    },
+  });
 
   return ability;
 };
--- packages/auth/src/permissions.ts
+++ packages/auth/src/permissions.ts
@@ -1,8 +1,7 @@
 import { AbilityBuilder } from '@casl/ability';
 import { AppAbility } from '.';
 import { User } from './models/user';
-
-type Role = 'ADMIN' | 'MEMBER';
+import { Role } from './roles';
 
 type PermissionsByRole = (
   user: User,
@@ -14,7 +13,9 @@ export const permissions: Record<Role, PermissionsByRole> = {
     can('manage', 'all');
   },
 
-  MEMBER: (_, { can }) => {
-    can('invite', 'User');
+  MEMBER: (user, { can }) => {
+    can(['create', 'get'], 'Project');
+    can(['update', 'delete'], 'Project', { ownerId: { $eq: user.id } });
   },
+  BILLING: () => {},
 };
