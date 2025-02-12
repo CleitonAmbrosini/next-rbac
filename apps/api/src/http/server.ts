6d2571ffad7afe6f578f005b32610c6710c00a52
Cleiton Ambrosini <cleiton.ambro@gmail.com>
2025-02-11 22:16:31 -0300

    docs: Create api documentation based on OAS

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -1,6 +1,9 @@
 import fastifyCors from '@fastify/cors';
+import fastifySwagger from '@fastify/swagger';
+import fastifySwaggerUI from '@fastify/swagger-ui';
 import { fastify } from 'fastify';
 import {
+  jsonSchemaTransform,
   serializerCompiler,
   validatorCompiler,
   ZodTypeProvider,
@@ -11,6 +14,22 @@ const app = fastify().withTypeProvider<ZodTypeProvider>();
 
 app.setSerializerCompiler(serializerCompiler);
 app.setValidatorCompiler(validatorCompiler);
+
+app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: 'Next.js RBAC',
+      description: 'Full-stack app with multi-tenant and RBAC ',
+      version: '1.0.0',
+    },
+    servers: [],
+  },
+  transform: jsonSchemaTransform,
+});
+
+app.register(fastifySwaggerUI, {
+  routePrefix: '/docs'
+});
 
 app.register(fastifyCors);
 
