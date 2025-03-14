8cb64864abae6d49ea6faf3eee4f43c24836382c
Cleiton Ambrosini <cleiton.ambro@gmail.com>
2025-03-13 22:47:24 -0300

    feat: create route to reset login with Github

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -14,6 +14,7 @@ import { createAccound } from './routes/auth/create-account';
 import { getProfile } from './routes/auth/get-profile';
 import { requestPasswordRecover } from './routes/auth/request-password-recover';
 import { resetPassword } from './routes/auth/reset-password';
+import { authenticateWithGithub } from './routes/auth/authenticate-with-github';
 
 const app = fastify().withTypeProvider<ZodTypeProvider>();
 
@@ -47,6 +48,7 @@ app.register(authenticateWithPassword);
 app.register(getProfile);
 app.register(requestPasswordRecover);
 app.register(resetPassword);
+app.register(authenticateWithGithub);
 
 app.listen({ port: 3333 }).then(() => {
   console.log('HTTP server running');
